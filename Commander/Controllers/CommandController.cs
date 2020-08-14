5cf514d4123a718bae7fab7c723f6110882feede
Tiago Rodrigues <Tiago.rds85@hotmail.com>
2020-08-13 21:11:03 -0300

    Initial commit

---
+++ Commander/Controllers/CommandController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using Commander.Data;
+using Commander.Dtos;
+using Commander.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Commander.Controllers
+{
+    [ApiController]
+    [Route("api/commands")]
+    // [Route("api/[controller]")]
+    public class CommandController : ControllerBase
+    {
+        private readonly ILogger<CommandController> _logger;
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandController(ICommandRepo repository, IMapper mapper, ILogger<CommandController> logger)
+        {
+            this._repository = repository;
+            this._mapper = mapper;
+            this._logger = logger;
+        }
+
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
+        {
+            var commands = _repository.GetAllCommands();
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}", Name = "GetCommandById")]
+        public ActionResult<CommandReadDto> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem != null)
+            {
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));    
+            }
+            return NotFound(new { Message = "Aconteceu erro na sua solicitação." });
+        }
+
+        //POST api/commands
+        [HttpPost]
+        public ActionResult<CommandCreateDto> CreateCommand(CommandCreateDto commandCreate)
+        {
+            var commandModel = _mapper.Map<Command>(commandCreate);
+            _repository.CreateCommand(commandModel);
+            _repository.SaveChanges();
+            var dtoCommandRead = _mapper.Map<CommandReadDto>(commandModel);
+            return CreatedAtRoute(nameof(GetCommandById), new { Id = dtoCommandRead.Id }, dtoCommandRead);
+        }
+
+        //PUT api/command/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdate)
+        {
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if (commandModelFromRepo == null)
+                return NotFound();
+
+            _mapper.Map(commandUpdate, commandModelFromRepo);
+            _repository.UpdateCommand(commandModelFromRepo);
+            _repository.SaveChanges();
+            return NoContent();
+        }
+
+        //PATCH api/commands/{id}
+        [HttpPatch("{id}")]
+        public ActionResult PartialCommandUpdate(int id, JsonPatchDocument<CommandUpdateDto> patchDoc)
+        {
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if (commandModelFromRepo == null)
+                return NotFound();
+            var commandToPatch = _mapper.Map<CommandUpdateDto>(commandModelFromRepo);
+            patchDoc.ApplyTo(commandToPatch, ModelState);
+            if (!TryValidateModel(commandToPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
+            _mapper.Map(commandToPatch, commandModelFromRepo);
+            _repository.UpdateCommand(commandModelFromRepo);
+            _repository.SaveChanges();
+            return NoContent();
+        }
+
+        //DELETE api/commands/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCommand(int id)
+        {
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if (commandModelFromRepo == null)
+                return NotFound();
+            _repository.DeleteCommand(commandModelFromRepo);
+            _repository.SaveChanges();
+            return NoContent();            
+        }
+    }
+}
